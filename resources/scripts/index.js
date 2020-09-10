7ba561dd7e40d5efa87437c8b1c997a8b40951b4
tranjg <tranjg@mail.uc.edu>
2020-09-10 17:04:18 -0400

    finalized game (copied from previous repository because of issues)

--- resources/scripts/index.js
+++ resources/scripts/index.js
@@ -1,12 +1,12 @@
 // Elements
-const welcomeScreen = document.getElementById(`welcome-screen`);
-const gameScreen = document.getElementById(`game-screen`);
-const startGameButton = document.getElementById(`start-game-button`);
-const userName = document.getElementById(`username`);
-const userSelection = document.getElementById(`user-selection`);
-const goButton = document.getElementById(`go-button`);
-const scoreParagraph = document.getElementById(`score`);
-const gameHistoryParagraph = document.getElementById(`game-history`);
+const welcomeScreen = document.querySelector(`#welcome-screen`);
+const gameScreen = document.querySelector(`#game-screen`);
+const startGameButton = document.querySelector(`#start-game-button`);
+const userName = document.querySelector(`#username`);
+const userSelection = document.querySelector(`#user-selection`);
+const goButton = document.querySelector(`#go-button`);
+const scoreParagraph = document.querySelector(`#score`);
+const gameHistoryParagraph = document.querySelector(`#game-history`);
 
 // instantiate the game object from the `RockPaperScissors` class.
 let game;
@@ -16,20 +16,19 @@ gameScreen.classList.add(`d-none`);
 
 // updateScoreTallyUI
 function updateScoreTallyUI(){
-  scoreParagraph.innerHTML = game.username + `: ` + game.score.user + ` v CPU: ` + game.score.cpu;
+  scoreParagraph.innerHTML = `<p>${userName.value} : ${game.score.user} v CPU: ${game.score.cpu}</p>`;
 }
 
 
 // updateGameHistoryUI
 
 function updateGameHistoryUI(){
-  gameHistoryParagraph.innerHTML = this.gameHistoryLog;
+  gameHistoryParagraph.innerHTML = game.gameHistoryLog;
 }
 
 // start-game-button EventListener
 startGameButton.addEventListener(`click`, function () {
-  const username = userName;
-  game = new RockPaperScissors(username);
+  game = new RockPaperScissors(userName.value);
   welcomeScreen.classList.add(`d-none`);
   gameScreen.classList.remove(`d-none`);
   // Complete
@@ -37,7 +36,7 @@ startGameButton.addEventListener(`click`, function () {
 
 // go-button EventListener
 goButton.addEventListener(`click`, function () {
-  game.play(userSelection);
+  game.play(userSelection.value);
   updateScoreTallyUI();
   updateGameHistoryUI();
 });
