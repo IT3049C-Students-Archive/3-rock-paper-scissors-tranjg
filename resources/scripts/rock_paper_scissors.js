98cb1140974505199c8e5b7d64dc233ed944ddb1
tranjg <tranjg@mail.uc.edu>
2020-09-10 17:03:57 -0400

    complete game logic; (copy over from repository because of problems)

--- resources/scripts/rock_paper_scissors.js
+++ resources/scripts/rock_paper_scissors.js
@@ -1,9 +1,9 @@
-class RockPaperScissors {
+class RockPaperScissors{
   constructor(username) {
     this.username = username;
     this.score = {
       user: 0,
-      cpu:0 
+      cpu:0
     },
     this.gameHistoryLog = [];
   }
@@ -23,11 +23,11 @@ class RockPaperScissors {
    * returns one of the following values: `win`, `lose`, `tie`
    * tie:
    *     the user selection the same as the CPU
-   * win: 
+   * win:
    *    (user is `rock` and cpu is `scissors
    *     OR
-   *    (user is `paper` and cpu is `rock`) 
-   *     OR 
+   *    (user is `paper` and cpu is `rock`)
+   *     OR
    *    (user is `scissors` and cpu is `paper`)
    * `lose`:
    *    the opposite case :)
@@ -46,22 +46,19 @@ class RockPaperScissors {
   }
 
   /**
-   * 
+   *
    * @param {string} userSelection user selection. Can only be one of the following values [`rock`, `paper`, `scissors`]
    */
   play(userSelection){
     //return this.determineWinner(userSelection, this.generateCPUResponse());
     let CPUResponse = this.generateCPUResponse();
-    this.determineWinner();
-    if (this.determineWinner() === `win`){
+    let outcome = this.determineWinner(userSelection, CPUResponse);
+    if (outcome === `win`){
       this.score.user ++;
-      this.gameHistoryLog.push(this.username + ` selected` + userSelection + ` CPU selected` + CPUResponse + ` ` + userSelection + ` wins`);
-    } else if (this.determineWinner() === `lose`){
+    } else if (outcome === `lose`){
       this.score.cpu ++;
-      this.gameHistoryLog.push(this.username + ` selected` + userSelection + ` CPU selected` + CPUResponse + ` ` + userSelection + ` loses`);
-    } else {
-      this.gameHistoryLog.push(this.username + ` selected` + userSelection + ` CPU selected` + CPUResponse + ` ` + userSelection + ` ties`);
     }
+    this.gameHistoryLog.push(`<br>${this.username} selected ${userSelection}, CPU selected ${CPUResponse}: ${this.username} ${outcome}s.`);
   }
 
 }
